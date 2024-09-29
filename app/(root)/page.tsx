8c39ef5c9976e5b066c300aef91a85bfd13e6038
Amy Arora <amyarora2214@gmail.cpm>
2024-09-30 00:56:20 +0530

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox';
+// import RecentTransactions from '@/components/RecentTransactions';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
+// import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
+// import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = () => {
+const Home = async () => {
+//   const currentPage = Number(page as string) || 1;
+  const loggedIn = { firstName : 'Amy',lastName: 'Arora',email:'amy22@gmail.com'};
+//   const accounts = await getAccounts({ 
+//     userId: loggedIn.$id 
+//   })
+
+//   if(!accounts) return;
+  
+//   const accountsData = accounts?.data;
+//   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+//   const account = await getAccount({ appwriteItemId })
+
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        {/* <RecentTransactions 
+          accounts={[]}
+          transactions={1}
+          appwriteItemId={appwriteItemId}
+          page={currentPage}
+        /> */}
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:1235.5},{currentBalance:4560.56}]}
+      />
+    </section>
   )
 }
 
